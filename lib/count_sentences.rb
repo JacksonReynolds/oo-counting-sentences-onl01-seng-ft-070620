6d05c01e18352035235ec6fcfbba8308a3b94bd1
Jackson Reynolds <jwreynold@gmail.com>
2020-07-13 12:38:23 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -17,5 +17,10 @@ class String
   def count_sentences
     binding.pry
     split_sentence = self.split('.')
+    split_sentence.each_with_index do |str, index|
+      if str.include?("?")
+        split_sentence[index] = str.split('?')
+      end #do
+    split_sentence.length
   end
 end
